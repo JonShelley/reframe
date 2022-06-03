19e761fe4f25f4d8a6487fd8650559fbe5d59348
Jon Shelley <jshelley.menan@gmail.com>
2022-06-03 17:54:52 +0000

    Updated the collected data

--- azure_nhc/network/ib/ib_count.py
+++ azure_nhc/network/ib/ib_count.py
@@ -15,7 +15,7 @@ class IBCardCheck(rfm.RunOnlyRegressionTest):
     descr = 'Check the number of IB cards'
     valid_systems = ['*']
     valid_prog_environs = ['*']
-    executable = 'ibstat -l'
+    executable = 'ibstat'
 
 
     @sanity_function
@@ -30,15 +30,23 @@ class IBCardCheck(rfm.RunOnlyRegressionTest):
         # Ideas for refactoring:
         # - get each name from ibstat -l and then check each one with ibstatus
         #   to see if the link_layer is InfiniBand or Ethernet (AccelNet)
-        ib_count = sn.extractall(
-            r'(mlx5_ib[0-9]+)', self.stdout
-            #r'(?P<name>\S+)', self.stdout
-        )
+        regex = r'CA\s+(?P<device_name>\S+)(.|\n)*?State:\s+(?P<device_state>\S+)(.|\n)*?Rate:\s+(?P<device_rate>\S+)(.|\n)*?Link layer:\s+(?P<device_type>\S+)'
+        ib_count = sn.extractall( regex, self.stdout, "device_name", str)
+        ib_state = sn.extractall( regex, self.stdout, "device_state", str)
+        ib_rate = sn.extractall( regex, self.stdout, "device_rate", str)
+        device_ll = sn.extractall( regex, self.stdout, "device_type", str)
         print("IB Cards: {}".format(ib_count))
+        print("IB State: {}".format(ib_state))
+        print("IB Rate: {}".format(ib_rate))
+        print("Link Layer: {}".format(device_ll))
         print("Count: {}".format(sn.count(ib_count)))
+
+        # Loop through the devices found and verify that they properly report their values.
+        for ib in ib_count:
+            print("IB: {}".format(ib))
         #return sn.assert_eq(sn.count(ib_count), 1 )
         #print("=====================")
-        pprint.pprint(vars(self.current_system))
+        #pprint.pprint(vars(self.current_system))
         #print("=========------------============")
         if vm_info != None and 'nhc_values' in vm_info and "ib_count" in vm_info['nhc_values']:
             return sn.assert_eq(sn.count(ib_count), vm_info['nhc_values']['ib_count'])
