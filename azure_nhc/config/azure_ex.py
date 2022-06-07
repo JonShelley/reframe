b619b9162b27f2028404bcc16cf1eeaaa46c1ed5
Jon Shelley <JonShelley@users.noreply.github.com>
2022-06-07 17:51:06 -0600

    Update azure_ex.py
    Updated gcc version to work with Azure Alma-HPC marketplace image

--- azure_nhc/config/azure_ex.py
+++ azure_nhc/config/azure_ex.py
@@ -107,7 +107,7 @@ site_configuration = {
         },
         {
             'name': 'gnu-azhpc',
-            'modules': ['gcc-9.2.0', 'mpi/hpcx'],
+            'modules': ['gcc-9.2.1', 'mpi/hpcx'],
             'cc': 'gcc',
             'cxx': 'g++',
             'ftn': 'gfortran'
