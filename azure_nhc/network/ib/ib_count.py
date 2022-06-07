f2f56802c0832c4f2a27b59e6af2cf9834fa1d4b
Jon Shelley <jshelley.menan@gmail.com>
2022-06-07 03:39:34 +0000

    adjusted ib_count

--- azure_nhc/network/ib/ib_count.py
+++ azure_nhc/network/ib/ib_count.py
@@ -81,8 +81,8 @@ class IBCardCheck(rfm.RunOnlyRegressionTest):
             return sn.all(
                 sn.assert_eq(sn.count(ib_names), vm_info['nhc_values']['ib_count']),
                 sn.all(sn.map(lambda x: sn.assert_eq(x, vm_info['nhc_values']['ib_rate']),ib_rates)),
-                sn.all(sn.map(lambda x: sn.assert_eq(x, vm_info['nhc_values']['ib_states']),ib_states)),
-                sn.all(sn.map(lambda x: sn.assert_eq(x, vm_info['nhc_values']['ib_pstates']),ib_pstates))
+                sn.all(sn.map(lambda x: sn.assert_eq(x, vm_info['nhc_values']['ib_state']),ib_states)),
+                sn.all(sn.map(lambda x: sn.assert_eq(x, vm_info['nhc_values']['ib_pstate']),ib_pstates))
             )
         else:
             print("ib_count not found in vm_info['nhc_values']")
